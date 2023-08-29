c8d751a757c5af36ea06c19cd5adedf00ee63184
Michał Robakowski <48829502+michaus736@users.noreply.github.com>
2023-08-29 20:25:21 +0200

    Add project files.

---
+++ TypeMaster/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TypeMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Style = (Style)FindResource("HoverButtonStyle");
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Style = (Style)FindResource("NormalButtonStyle");
+        }
+
+        private void GetDifficultyView(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
